a0893ebfea1dee63c26b13a06f9f2878d12463f9
snew430 <seanpnew@hotmail.com>
2022-02-04 13:04:18 -0500

    user prompts for project

--- app.js
+++ app.js
@@ -1,18 +1,152 @@
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
 
-inquirer
-.prompt([
-  {
-    type: "input",
-    name: "name",
-    message: "What is your name?"
+const promtUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username (Required)",
+      validate: (gitInput) => {
+        if (gitInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmAbout",
+      message:
+        "Would you like to enter some information about yourself for an 'About' section?",
+      default: true,
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself",
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+    },
+  ]);
+};
+
+const promptProject = (portfolioData) => {
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
   }
-])
-.then(answers => console.log(answers))
+
+  console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your project? (Required)",
+        validate: (projectName) => {
+          if (projectName) {
+            return true;
+          } else {
+            console.log("Please enter your Project name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Provide a description of the project (Required)",
+        validate: (projectDescription) => {
+          if (projectDescription) {
+            return true;
+          } else {
+            console.log("Please enter a description of your project!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "checkbox",
+        name: "languages",
+        message: "What did you build this project with? (Check all that apply)",
+        choices: [
+          "JavaScript",
+          "HTML",
+          "CSS",
+          "ES6",
+          "jQuery",
+          "Bootstrap",
+          "Node",
+        ],
+      },
+      {
+        type: "input",
+        name: "link",
+        message: "Enter the GitHub link to your project. (Required)",
+        validate: (githubLink) => {
+          if (githubLink) {
+            return true;
+          } else {
+            console.log("Please enter your GitHub link!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "confirm",
+        name: "feature",
+        message: "Would you like to feature this project?",
+        default: false,
+      },
+      {
+        type: "confirm",
+        name: "confirmAddProject",
+        message: "Would you like to enter another project?",
+        default: false,
+      },
+    ])
+    .then((projectData) => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
+
+promtUser()
+  .then(promptProject)
+  .then((portfolioData) => {
+    console.log(portfolioData);
+  });
+
 // const fs = require("fs");
 // const generatePage = require("./src/page-template.js")
 // const pageHTML = generatePage(name, github)
-
 
 // fs.writeFile("index.html", generatePage(name, github), (err) => {
 //   if (err) throw err;
