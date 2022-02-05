da1d996e5bf8c5a9eac7eb345855f1d787d430fd
snew430 <seanpnew@hotmail.com>
2022-02-04 19:19:24 -0500

    added funtionality for feature repos

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 const inquirer = require("inquirer");
 
 const promtUser = () => {
@@ -141,14 +143,12 @@ const promptProject = (portfolioData) => {
 promtUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("index.html", generatePage(portfolioData), (err) => {
+      if (err) throw err;
+      console.log(
+        "Portfolio complete! Check out index.html to see the output!"
+      );
+    });
   });
-
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js")
-// const pageHTML = generatePage(name, github)
-
-// fs.writeFile("index.html", generatePage(name, github), (err) => {
-//   if (err) throw err;
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
